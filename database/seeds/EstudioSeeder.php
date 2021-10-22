9cdb98f9abfb67c16cf4d7a0dcd9a2a8add8eeb7
Denisdif <ferreiradenisiv@gmail.com>
2021-10-21 21:50:51 -0300

    Se modificó el seeder de Estudios

--- database/seeds/EstudioSeeder.php
+++ database/seeds/EstudioSeeder.php
@@ -14,204 +14,253 @@ class EstudioSeeder extends Seeder
     {
     // Bioquimico
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Acido Urico',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Acido Trasmuconico Urinario',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Chagas',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Coagulograma',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Colesterol HDL',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Colesterol LDL',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Citologia Hormonal y Oncologica',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Creatininemia',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Coproparasitologico',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Test Embarazo / gravindex',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Exudado Vaginal',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Exudado uretral',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Glutamil Transpeptidasa',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Grupo Sanguineo y Factor RH',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'sub unidad HGB cuantitativa sangre femenino',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Hemoaglutinacion Indirecta (Ac. Anti Trpanosoma Cruzi) con 2 ME - Sin 2 EM',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Hemoglobina Glicocilada',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Hepatograma',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Hidroxipireno en orina',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'HIV',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Ionograma',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Lipidograma',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Proteinas Totales',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'PSA',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Reaccion Mantoux',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Reaccion Hudleson',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Recuento de Plaquetas',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Recuento Reticulositos',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Serologia Hepatitis A',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Serologia Hepatitis B',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Serologia Hepatitis C',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'TSH',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Toxoplasmosis',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Toxicologico: Marihuana',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Toxicologico: Cocaina',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Toxicologico: Anfetaminas',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Toxicologico:
             Benzodiacepinas',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Toxicologico: Alcolemia en
             sangre',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Triglicéridos',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Uricemia (acido urico)',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Urocultivo (femenino)',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Urea',
             'tipo_estudio_id' => 1,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'VDRL',
             'tipo_estudio_id' => 1,
         ]);
     //
     // Anexo 01
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Colesterol Total
             (Colesterolemia)
             ',
             'tipo_estudio_id' => 2,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Eritrosedimentacion',
             'tipo_estudio_id' => 2,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Glucemia',
             'tipo_estudio_id' => 2,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Hemograma',
             'tipo_estudio_id' => 2,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Orina completa',
             'tipo_estudio_id' => 2,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'Uremia',
             'tipo_estudio_id' => 2,
         ]);
@@ -219,47 +268,57 @@ class EstudioSeeder extends Seeder
     //
     // Complementarios
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'AUDIOMETRIA',
             'tipo_estudio_id' => 3,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ELECTROCARDIOGRAMA',
             'tipo_estudio_id' => 3,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ELECTROENCEFALOGRAMA',
             'tipo_estudio_id' => 3,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ESPIROMETRIA',
             'tipo_estudio_id' => 3,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'SENSOMETRICO',
             'tipo_estudio_id' => 3,
         ]);
     //
     // Ex Clinico
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'DECLARACION JURADA',
             'tipo_estudio_id' => 4,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'HISTORIA CLINICO',
             'tipo_estudio_id' => 4,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'CUESTIONARIO
             DIRECCIONADO POSICI FZA',
             'tipo_estudio_id' => 4,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'CUESTIONARIO DIRECCIONADO ILUMINACION',
             'tipo_estudio_id' => 4,
         ]);
     //
     //Psicotecnico
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ORIENTADO A CONDUCCION
             DE VEHICULOS,
             EQUIPAMIENTO Y
@@ -267,47 +326,57 @@ class EstudioSeeder extends Seeder
             'tipo_estudio_id' => 5,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ORIENTADO A ALTURA',
             'tipo_estudio_id' => 5,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ORIENTADO A ESPACIO
             CONFINADO',
             'tipo_estudio_id' => 5,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ORIENTADO A SITUACIONES
             DE EMERGENCIA',
             'tipo_estudio_id' => 5,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'ORIENTADO A LA DOCENCIA',
             'tipo_estudio_id' => 5,
         ]);
     //
     // Radiologia
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'RX TORAX',
             'tipo_estudio_id' => 6,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'RX COLUMNA LUMBAR (FYP)',
             'tipo_estudio_id' => 6,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'RX COLUMNA CERVICAL
             (FYP)',
             'tipo_estudio_id' => 6,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'RX MANOS',
             'tipo_estudio_id' => 6,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'RX RODILLAS',
             'tipo_estudio_id' => 6,
         ]);
         $estudio = Estudio::create([
+            'carga' => true,
             'nombre' => 'RX HOMBROS',
             'tipo_estudio_id' => 6,
         ]);
