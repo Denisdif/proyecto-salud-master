e5c6bfc811d00d87c915a7d3f26b17bfb22ec39a
Denisdif <ferreiradenisiv@gmail.com>
2021-10-28 00:59:47 -0300

    Se desarrollo el modelo del formulario de Iluminacion Cuestionario Direccionado

--- app/Models/IluminacionDireccionado.php
+++ app/Models/IluminacionDireccionado.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class IluminacionDireccionado extends Model
 {
-    //
+    protected $fillable = [ 'puesto',
+                            'antiguedad',
+                            'direccion_completa',
+                            'observaciones',
+                            'voucher_id',
+                            'enfermedades',
+                            'transtornos_congenitos',
+                            'enfermedades_profecionales',
+                            'exposicion_anterior',
+                            'exposicion_actual',
+                            'cefaleas',
+                            'vision_doble',
+                            'mareo_vertigo',
+                            'conjuntivitis',
+                            'vision _borrosa',
+                            'inseguridad_de_pie',
+                            'no_centrados',
+                            'pupilas_anormales',
+                            'conjuntivas_anormales',
+                            'corneas_anormales',
+                            'motilidad_anormal',
+                            'nistagmus_ausente',
+                            'informe_ocular',
+                            'av_correccion',
+                            'av_sin_correccion'];
+
+    public function voucher()
+    {
+        return $this->hasOne('App\Voucher', 'id', 'voucher_id');
+    }
 }
